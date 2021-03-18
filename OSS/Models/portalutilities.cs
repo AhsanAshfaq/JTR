c671764d086f406e83cde6282cd1682c5e2952c5
faizan <faizanusman101@gmail.com>
2021-03-18 18:15:09 +0500

    Generate fees Done but not test

--- OSS/Models/portalutilities.cs
+++ OSS/Models/portalutilities.cs
@@ -10,38 +10,55 @@ using OSS.Models;
 
 namespace OSS.Models
 {
-                    //Session["employee"] = emp;
-                    //Session["UserID"] = emp.UserID;
-                    //Session["name"] = emp.UserName;
-                    //Session["schollid"] = emp.SchoolID;
-                    //Session["roleid"] = emp.RoleID;
-    
+    //Session["employee"] = emp;
+    //Session["UserID"] = emp.UserID;
+    //Session["name"] = emp.UserName;
+    //Session["schollid"] = emp.SchoolID;
+    //Session["roleid"] = emp.RoleID;
+
     static class portalutilities
     {
-        
-      //  public static int _schollid = Convert.ToInt32(System.Web.HttpContext.Current.Session["schollid"]);
+
+        //  public static int _schollid = Convert.ToInt32(System.Web.HttpContext.Current.Session["schollid"]);
+       
+        public static int ActiveSessionID = 1;
         public static int _schollid = 1;
+        public static bool InEnglish = true;
         //public static string _username =(System.Web.HttpContext.Current.Session["name"]).ToString();
         public static string _username = "admin1";
         public static DateTime _GetDate = DateTime.Now;
         //public static int _RoleID = Convert.ToInt32(System.Web.HttpContext.Current.Session["roleid"]);
         public static int _RoleID = 1;
 
-        //var obj = (from q in cntx.tblStudentRegMst
-        //           join a in cntx.tblAdmission on q.RegID equals a.RegID
-        //           join c in cntx.Vu_Class on a.AdmissionID equals c.AdmissionID
-        //           where q.IsActive == true && q.IsDelete == false && q.SchoolID == PortalUtilities.loginschoolid
-        //           && c.StageID == StgID && c.SessionID == PortalUtilities.ActiveSessionID
-        //           select new
-        //           {
-        //               a.AdmissionID,
-        //               a.GRNo,
-        //               q.ApplicantName,
-        //               q.ApplicantNameInUrdu,
-        //               q.FatherName,
-        //               q.FatherNameInUrdu
-        //           }).ToList();
+        #region Fee Module
+
+        public static string MonthYearFomrat = "MMM-yyyy";
+
+        public struct DiscountType
+        {
+            public static string Percentage = "Percentage";
+            public static string Fixed = "Fixed";
+        }
+
+        public struct FeesStatus
+        {
+            public static string Paid = "Paid";
+            public static string UnPaid = "UnPaid";
+            public static string Partial = "Partial";
+            public static string Received = "Received";
+        }
+
+        public struct ChargeFee
+        {
+            public static string OneTime = "One Time";
+            public static string Monthly = "Monthly";
+            public static string HalfYearly = "Half Yearly";
+            public static string Quaterly = "Quaterly";
+            public static string Yearly = "Yearly";
+        }
+
+        #endregion
 
     }
-    
+
 }
