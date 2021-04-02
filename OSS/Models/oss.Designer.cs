3597116974e6998cda1d5b515218eb25780a5c36
Ahsan A <ahsan.a@allshorestaffing.com>
2021-04-02 17:05:42 +0500

    added config changes

--- OSS/Models/oss.Designer.cs
+++ OSS/Models/oss.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Google Drive\Projects\faizan\MVC OSS\OSS\OSS\Models\oss.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Faizan School Project\Project\JTR\OSS\Models\oss.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
