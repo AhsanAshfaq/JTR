ff97044bf98f7eb6d686f3469810ec8a61a91377
Ahsan A <ahsan.a@allshorestaffing.com>
2021-04-02 16:54:39 +0500

    added test reporting

---
+++ OSS/Controllers/FeeVoucherController.cs
@@ -0,0 +1,66 @@
+﻿using iText.Html2pdf;
+using OSS.Models;
+using OSS.Models.viewmodel;
+using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSS.Controllers
+{
+    public class FeeVoucherController : Controller
+    {
+        // GET: FeeVoucher
+        private OssEntities db = new OssEntities();
+        public ActionResult Create()
+        {
+            ViewBag.StageList = db.tblStage.Where(o => o.SchoolID == portalutilities._schollid && o.IsDelete == false && o.IsActive == true).ToList();
+            return View();
+        }
+        public ActionResult LoadStudents(int stageId, int classId, int sectionId, string selectedPeriod, string activeness) 
+        {
+            var llist = db.tblAdmission.Where(x => x.tblStudentRegMst.SchoolID == portalutilities._schollid && x.tblStudentRegMst.StageID == stageId 
+                            && x.tblStudentRegMst.ClassID == classId && x.tblStudentRegMst.SectionID == sectionId).ToList();
+
+
+            var result = new List<object>();
+            foreach (var item in llist)
+            {
+                result.Add(new
+                {
+                    AdmissionId = item.AdmissionID,
+                    StudentName = portalutilities.InEnglish ? item.tblStudentRegMst.ApplicantName : item.tblStudentRegMst.ApplicantNameInUrdu,
+                    GrNo = item.GRNo
+                });
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult FeeVoucherReport(string studentIds)
+        {
+            var model = new FeeVoucherReportModel
+            {
+                AdmissionId = 1,
+                StudentName = "Test",
+                StudentFatherName = "Test",
+                ClassName = "Owla",
+                GrNo = "1440/0321/0005",
+                SectionName = "AWWAL",
+                RollNumber = "13",
+                SchoolAddress = "PLOT # 1,ST 4, PHASE1, SECTOR 4, SCHEME 33 AHSANABAD KARACHI",
+                SchoolLogoUrl = "https://ibb.co/Sv0GJRW",
+                SchoolName = "JAMIA-TUR-RASHEED",
+                SchoolPhone = "03340555850"
+
+            };
+            HtmlConverter.ConvertToPdf("<h1>Test</h1>", new FileStream(@"c:\>File.pdf", FileMode.CreateNew));
+            return View(model);
+        }
+        public ActionResult GenerateReport(string studentIds) 
+        {
+            return RedirectToAction("FeeVoucherReport", new { studentIds = studentIds });
+        }
+    }
+}
---
+++ OSS/Models/viewmodel/FeeVoucherReportModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace OSS.Models.viewmodel
+{
+    public class FeeVoucherReportModel
+    {
+        public int? AdmissionId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentFatherName { get; set; }
+        public string ClassName { get; set; }
+        public string SectionName { get; set; }
+        public string GrNo { get; set; }
+        public string RollNumber { get; set; }
+        public string SchoolName { get; set; }
+        public string SchoolAddress { get; set; }
+        public string SchoolPhone { get; set; }
+        public string SchoolLogoUrl { get; set; }
+        public List<FeeVoucherFees> feeList { get; set; }
+
+    }
+    public class FeeVoucherFees 
+    {
+        public string FeeType { get; set; }
+        public string FeeMonth { get; set; }
+        public int Amount { get; set; }
+    }
+}
